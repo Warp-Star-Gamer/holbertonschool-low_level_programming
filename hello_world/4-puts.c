13ae60505d2a8692c36a4ac2d809c85f1f35c4dc
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-19 15:19:16 +0000

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #!/bin/bash
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
+int main( int argc, char * argv[] ) {
+    puts( "Programming is like building a multilingual puzzle" );        
     return 0;
 }
