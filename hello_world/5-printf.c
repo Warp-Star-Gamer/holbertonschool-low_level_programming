d23e47a795ef10c0896522d906bb26958b4e4b5a
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-20 10:17:39 +0000

    git config --global user.name Warp-Star-Gamer

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - call void
+ * puts - print
+ * Return: (success)
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
