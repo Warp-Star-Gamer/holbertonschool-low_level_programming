11f2d31462af40c4fde316397d9881fbc580d2b0
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-25 19:46:52 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: Assigns a random number to variable n and prints its last digit
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter += 1)
+{
+putchar(letter);
+if (letter != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
