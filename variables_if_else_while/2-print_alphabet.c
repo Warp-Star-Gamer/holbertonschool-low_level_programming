bfec0fe8ceab60e96d044e11b85b3d61bb88e5c7
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-25 19:14:38 +0000

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: Assigns a random number to variable n and prints its last digit
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 char letter;
@@ -7,5 +13,6 @@ for (letter = 'a'; letter <= 'z'; letter += 1)
 {
 	putchar(letter);
 }
+putchar('\n');
 return (0);
 }
