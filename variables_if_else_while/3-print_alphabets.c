fe89e71322c6314ac5e1a2aa5c6f8e7a84b8e9a0
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-25 19:19:27 +0000

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: Assigns a random number to variable n and prints its last digit
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter += 1)
+{
+putchar(letter);
+}
+for (letter = 'A'; letter <= 'Z'; letter += 1)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
