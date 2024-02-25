58a990ca18997e1eb7abeeb0477ed91b77cd47f9
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-25 19:26:54 +0000

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: Assigns a random number to variable n and prints its last digit
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter += 1)
+{
+if (letter != 'q' && letter != 'e')
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
