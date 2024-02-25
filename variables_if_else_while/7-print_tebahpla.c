71cbed5659cac451b53cbcfbef52fc73033eb3cc
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-25 19:37:46 +0000

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: Assigns a random number to variable n and prints its last digit
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter -= 1)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
