d4088bd40a4ba4fa8d94fcea2a0d0549933d6594
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-26 20:06:38 +0000

    The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,16 @@
 #include "main.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
 * print_numbers - prints numbers
 * Return: void
 */
 void print_numbers(void)
 {
-printf("0123456789\n");
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
