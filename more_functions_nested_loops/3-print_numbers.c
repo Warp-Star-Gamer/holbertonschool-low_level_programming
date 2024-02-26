ae68f44bd8bc07928263e9fd353a1fa2249a4c62
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-26 19:51:41 +0000

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - prints numbers
+* Return: void
+*/
+void print_numbers(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _isupper (int C);
 int _isdigit (int C);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
