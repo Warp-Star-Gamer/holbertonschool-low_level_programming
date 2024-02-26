15a7806e6ca22cecf91aac380f018e16db810628
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-26 19:20:21 +0000

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isdigit - is uppercase
+* @c: character
+* Return: 1 if c is uppercase, otherwise return 0.
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
