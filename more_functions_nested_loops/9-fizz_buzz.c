03aedba301eccd57d02a5f08ed21cc389ba555ac
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-27 19:03:54 +0000

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints square
+* Return: integer
+*/
+
+int main(void)
+{
+int i, i3, i5;
+for (i = 1; i <= 100; i++)
+{
+i3 = i % 3;
+i5 = i % 5;
+if (i3 != 0 && i5 != 0)
+printf("%d", i);
+if (i3 == 0)
+printf("Fizz");
+if (i5 == 0)
+printf("Buzz");
+printf(" ");
+}
+return (0);
+}
