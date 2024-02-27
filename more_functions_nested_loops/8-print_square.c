f8b6b6d6fc1bd85be0909cb8cfad66199c6abca1
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-27 18:49:19 +0000

    You are so much sunshine in every square inch

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,21 @@
-asd
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_square - prints square
+* @size: integer
+* Return: void
+*/
+
+void print_square(int size)
+{
+int x, y;
+for (y = 0; y < size; y++)
+{
+for (x = 0; x < size; x++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
