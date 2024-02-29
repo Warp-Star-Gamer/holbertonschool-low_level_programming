fa00c71d35b197a1afd4e85be69aa80ffb9224c6
Warper <71822478+Warp-Star-Gamer@users.noreply.github.com>
2024-02-29 19:09:15 +0100

    Create 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strcpy - copies a string to another
+* @dest: string
+* @src: string
+* Return: string
+*/
+char* _strcpy(char *dest, char *src)
+{
+char *cpy = dest;
+while (*src)
+*cpy++ = *src++;
+*cpy = 0;
+return (dest);
+}
