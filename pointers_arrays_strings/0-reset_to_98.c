57ddcd262607ff942817ac5935194e17f0e90269
Warp-Star-Gamer <igor.condolo@gmail.com>
2024-02-29 16:04:42 +0000

    98 Battery st

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-* function reset_to_98 - sets param to 98
-* #n: integer
+* reset_to_98 - resets to 98
+* @n: integer pointer
 * Return: void
 */
 void reset_to_98(int *n)
