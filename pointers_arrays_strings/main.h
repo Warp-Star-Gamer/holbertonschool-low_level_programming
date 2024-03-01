3faaf7398ef75c140351612d003f4bad71161c66
Warper <71822478+Warp-Star-Gamer@users.noreply.github.com>
2024-03-01 19:02:15 +0100

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,9 +10,6 @@ void print_array(int* a, int n);
 char* _strcpy(char *dest, char *src);
 int _atoi(char *s);
 void _putchar(int c);
-#include <stdio.h>
-#include <stdlib.h>
-#include <malloc.h>
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
