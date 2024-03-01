93fa3d1180c704bb231cbfb85dc2a7256063d782
Warper <71822478+Warp-Star-Gamer@users.noreply.github.com>
2024-03-01 18:33:39 +0100

    Create 7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+/**
+* leet - letters to numbers
+* @s: string
+* Return: string
+*/
+char *leet(char *s) {
+const char table[] = {'a','4','e','3','o','0','t','7','l','1'};
+char *head = s;
+int i;
+while (*s)
+{
+for (i = 0; i < sizeof(table); i += 2)
+{
+if (*s == table[i] || *s == table[i] + 'A' - 'a')
+{
+*s = table[i + 1];
+}
+}
+s++;
+}
+return (head);
+}
