586936d76be0cd4bd6155520757d1746edebc963
Warper <71822478+Warp-Star-Gamer@users.noreply.github.com>
2024-03-01 18:30:32 +0100

    Create 3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+/**
+* _strcmp - compares strings
+* @s1: string
+* @s2: string
+* Return: integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 == *s2++)
+if (*s1++ == '\0')
+return (0);
+return (*(unsigned char*)s1 - *(unsigned char*)(s2 - 1));
+}
