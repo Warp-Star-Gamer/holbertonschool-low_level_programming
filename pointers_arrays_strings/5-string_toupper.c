b190f4b028260d1dc81a6168960b324ad2de028a
Warper <71822478+Warp-Star-Gamer@users.noreply.github.com>
2024-03-01 18:32:17 +0100

    Create 5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+* string_toupper - puts to uuper case
+* @s: string
+* Return: string
+*/
+char* string_toupper(char *s)
+{
+char *head = s;
+while (*s)
+{
+if (*s >= 'a' && *s <= 'z')
+*s += 'A' - 'a';
+s++;
+}
+return (head);
+}
