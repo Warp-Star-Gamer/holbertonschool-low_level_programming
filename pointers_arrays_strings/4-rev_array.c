baf4225aecccf4d6181d997a9ff788dc03c4cc1b
Warper <71822478+Warp-Star-Gamer@users.noreply.github.com>
2024-03-01 18:31:54 +0100

    Create 4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+* reverse_array - puts n array upside-down
+* @a: integer array
+* @n: integer
+* Return: void
+*/
+void reverse_array(int *a, int n)
+{
+int *head = a;
+int *tail = a + n - 1;
+int temp;
+while (head < tail)
+{
+temp = *head;
+*head++ = *tail;
+*tail-- = temp;
+}
+}
