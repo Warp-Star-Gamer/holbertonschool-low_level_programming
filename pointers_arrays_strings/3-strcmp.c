62980c8a75c798411da4de26d9b316d6280b3506
fcondolo <fcondolo@users.noreply.github.com>
2024-03-03 20:50:25 +0100

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,5 +13,5 @@ int _strcmp(char *s1, char *s2)
 while (*s1 == *s2++)
 if (*s1++ == '\0')
 return (0);
-return (*(unsigned char*)s1 - *(unsigned char*)(s2 - 1));
+return (*(unsigned char *)s1 - *(unsigned char *)(s2 - 1));
 }
