d0289b18e3aade0bb9673bc2b7a45162488ba5ee
fcondolo <fcondolo@users.noreply.github.com>
2024-03-03 20:27:27 +0100

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -16,15 +16,15 @@ char *new_dest;
 char *write;
 while (*src_end)
 src_end++;
-while(*dst_end)
+while (*dst_end)
 dst_end++;
 new_dest = dest; /* secure version ==> (char*)malloc((src_end - src) + (dst_end - dest) + 1); */
-if(new_dest)
+if (new_dest)
 {
 write = new_dest;
-while(*dest)
+while (*dest)
 *write++ = *dest++;
-while(*src)
+while (*src)
 *write++ = *src++;
 *write = 0;
 }
